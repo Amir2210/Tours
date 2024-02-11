f6fff7a093775e10e4b612a1a5cff4b338b6d6b5
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-02-11 13:22:48 +0200

    Refactor the useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { ToursList } from './cmps/ToursList'
+import { Loader } from './cmps/Loader'
 
 const url = 'https://course-api.com/react-tours-project'
 
@@ -7,29 +8,37 @@ const App = () => {
   const [tours, setTours] = useState([])
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const data = await fetch(url)
-        const tours = await data.json()
-        setTours(tours)
-      } catch (error) {
-        console.log('error:', error)
-      }
-    }
     fetchData()
   }, [])
+
+  async function fetchData() {
+    try {
+      const data = await fetch(url)
+      const tours = await data.json()
+      setTours(tours)
+    } catch (error) {
+      console.log('error:', error)
+    }
+  }
 
   function onRemoveTour(tourId) {
     const newFilteredTours = tours.filter(tour => tour.id !== tourId)
     setTours(newFilteredTours)
   }
 
-  if (!tours) return <div>Loading...</div>
+  function refreshTours() {
+    fetchData()
+  }
+
+  if (!tours) return <Loader />
 
   return (
     <main>
-      <h2 className='title'>{tours.length ? 'Our Tours' : 'No Tours Left'}</h2>
-      <div className='title-underline'></div>
+      <div className="title">
+        <h2>{tours.length ? 'Our Tours' : 'No Tours Left'}</h2>
+        <div className='title-underline'></div>
+        {!tours.length && <button onClick={refreshTours} className="btn">refresh</button>}
+      </div>
       <ToursList tours={tours} onRemoveTour={onRemoveTour} />
     </main>
   )
