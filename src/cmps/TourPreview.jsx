12f7b18c6ff10df805a45e89d5571f1acb61df5c
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-02-11 13:01:27 +0200

    Render all the info about each tour

--- src/cmps/TourPreview.jsx
+++ src/cmps/TourPreview.jsx
@@ -1,7 +1,15 @@
 export function TourPreview({ tour }) {
+  const { id, name, info, image, price } = tour
   return (
-    <article>
-      <h1>{tour.name}</h1>
+    <article className='single-tour'>
+      <img className='img' src={image} alt={name} />
+      <span className='tour-price'>{price}</span>
+      <div className="tour-info">
+        <h5>{name}</h5>
+        <p>{info}</p>
+        <button className='info-btn'>read More</button>
+        <button className='delete-btn btn-block btn'>not interested</button>
+      </div>
     </article>
   )
 }
