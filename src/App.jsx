b76c8fc2286e17724b6c5515943306751bc6b568
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-02-11 12:38:13 +0200

    Fetching tours data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from 'react'
+
 const url = 'https://course-api.com/react-tours-project'
 
 const App = () => {
-  return <h2>Tours Starter</h2>
+  const [tours, setTours] = useState(null)
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await fetch(url)
+        const tours = await data.json()
+        console.log(tours)
+      } catch (error) {
+        console.log('error:', error)
+      }
+    }
+    fetchData()
+  }, [])
+
+  return (
+    <>
+
+    </>
+  )
 }
 export default App
